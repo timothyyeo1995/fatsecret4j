a1cd6e92955611888baf64962a2cb23a848b8ede
timothyyeo1995 <70092200+timothyyeo1995@users.noreply.github.com>
2022-09-14 23:17:39 +0800

    Update ResponseListener.java
    update onfoodresponse

--- src/main/java/com/fatsecret/platform/services/android/ResponseListener.java
+++ src/main/java/com/fatsecret/platform/services/android/ResponseListener.java
@@ -34,7 +34,7 @@ public interface ResponseListener {
 	 *
 	 * @param food			the food item from the response
 	 */
-	default public void onFoodResponse(Food food) {
+	default public HashMap<String,Object> onFoodResponse(Food food) {
 		System.out.println("ResponseListener onFoodResponse");
 	}
 	
