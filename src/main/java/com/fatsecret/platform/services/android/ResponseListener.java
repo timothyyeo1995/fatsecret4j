37fd6231a8bfca5de978af647694cf37d24bfc14
timothyyeo1995 <70092200+timothyyeo1995@users.noreply.github.com>
2022-09-14 23:44:39 +0800

    Update ResponseListener.java
    6th commit

--- src/main/java/com/fatsecret/platform/services/android/ResponseListener.java
+++ src/main/java/com/fatsecret/platform/services/android/ResponseListener.java
@@ -34,11 +34,9 @@ public interface ResponseListener {
 	 *
 	 * @param food			the food item from the response
 	 */
-	default public HashMap<String,Object> onFoodResponse(Food food) {
-        	System.out.println("ResponseListener onFoodResponse");
-        	HashMap<String, Object> hashmap = null;
-        	return hashmap;
-    	}
+	default public void onFoodResponse(Food food) {
+		System.out.println("ResponseListener onFoodResponse");
+	}
 	
 	/**
 	 * Called when a compact food list response is received.
